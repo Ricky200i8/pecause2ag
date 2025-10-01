2e5abb7bc3fd660de9baab77ce2a7860c56ccb66
ricardoariasgal87 <ricardoariasgal87@gmail.com>
2025-10-01 16:15:54 +0000

    💄ui(index): Asignar color de fondo para la aplicacion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack 
-  screenOptions={{
-    headerStyle: {
-      backgroundColor: '#424EF5',
-    },
-    headerShown:false,
-    statusBarStyle:'dark',
-  }}
-  />;
+  return <Stack />;
 }
